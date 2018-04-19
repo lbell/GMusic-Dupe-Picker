519cd4561c40c9f73470fab4b0a99e2435881fee
LBell <lbell270@gmail.com>
2018-04-19 11:55:48 -0600

    initial commit

---
+++ gmusic_dupe_picker.py
@@ -0,0 +1,110 @@
+# !/usr/bin/env python
+
+# NO WARRANTIES expressed or implied. This could completely delete everything!
+# (I don't see how it would, but if it does, I would like to know about it...)
+#
+# If you find this useful (it is extraordinarly useful) please consider a small
+# monitary hat tip: https://www.paypal.me/333221/5
+
+from gmusicapi import Mobileclient
+from getpass import getpass
+from collections import defaultdict
+from pick import pick
+
+
+client = Mobileclient()
+conn = False
+
+while not conn:
+    conn = client.login(str(raw_input('Username:')), getpass(), Mobileclient.FROM_MAC_ADDRESS)
+    print("\n")
+
+print('Getting updated list of all songs. (This may take a while)')
+all_songs = client.get_all_songs()
+
+####################################
+# Reduce api calls during dev and moderately speed up boot
+# ##################################
+# import pickle
+# with open('pickledsongs', 'wb') as fp:
+#     pickle.dump(all_songs, fp)
+#
+# with open('pickledsongs', 'rb') as fp:
+#     all_songs = pickle.load(fp)
+
+candidate_songs = defaultdict(list)
+album_list = defaultdict(int)
+dup_songs = {}
+trash_songs = {}
+
+print('Processing %s songs...' % (len(all_songs)))
+
+for song in all_songs:
+    # Title & Artist only to find same songs in different albums
+    key = "%s - by %s" % (song.get('title').encode("utf-8"), song.get('artist').encode("utf-8"))
+    key = key.title()
+
+    candidate_songs[key].append(song)
+    album_list[song.get('albumId')] += 1  # TODO Not consistent
+
+
+dict(candidate_songs)
+album_list[None] = 999  # placeholder
+
+for key in candidate_songs:
+    if len(candidate_songs[key]) > 1:
+        dup_songs[key] = candidate_songs[key]
+
+
+
+def get_label(song):
+    """Creates a "label" for options in pick by processesin song information
+    on the fly.
+
+    Note: changing this will require tweaking the "title" option for pick to
+    ensure the columns line up.
+    """
+    album = song.get('album').encode("utf-8")
+    albumartist = song.get('albumArtist').encode("utf-8")
+    album_count = album_list.get(song.get('albumId'))  # TODO inconsistent?
+    if album_count == 999:
+        album_count = ' UsrDef'
+    matched = 'Y' if song.get('nid') else 'N'
+    discnum = song.get('discNumber', 0)
+    tracknum = song.get('trackNumber', 0)
+    dur = int(song.get('durationMillis', 0))
+    length = "{}:{:02d}".format((dur / (1000 * 60)) % 60, (dur / 1000) % 60)
+    playcount = song.get('playCount', 0)
+
+    label = "{:2}-{:02d} | {:45s} | {:12s} |  {}  | {:7} | {:6} |  {}".format(
+        discnum, tracknum, album[:45], albumartist[:12], matched, album_count, length, playcount)
+
+    return label
+
+
+numdup = str(len(dup_songs))
+count = 1
+for item in sorted(dup_songs, key=lambda item: dup_songs[item][0].get('artist'), reverse=False):
+    options = dup_songs[item]
+    title = ("\n\n\n" +
+             "(" + str(count) + " of " + numdup + ")\n" +
+             "Select all of the songs to be DELETED using arrow and space. Hit ENTER to DELETE selected.\n" +
+             "(This is NOT reversable. There will be no warnings. Proceed with caution.)" +
+             "\n\n\n" +
+             item +
+             "\n\n" +
+             "   D-T  | {:45s} | {:12s} | nid | MySongs | Length | Play Count ").format("Album", "Album Artist")
+
+    selected = pick(options, title, multi_select=True, indicator='*', options_map_func=get_label)
+
+    if len(selected):
+        delete_ids = []
+        delete_songs = []
+        for item in selected:
+            delete_ids.append(item[0]['id'])
+            print("Deleting: [%s] from [%s]..." % (item[0]['title'], item[0]['album']))
+        client.delete_songs(delete_ids)
+
+    count += 1
+
+print('\n\nAll done. Bye!')
